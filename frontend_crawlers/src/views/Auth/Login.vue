b568abebc1050a5fecadf20dd5b25a69aafffa94
grinnbob <grifon12358@gmail.com>
2021-07-22 15:27:10 +0300

    redirect after login to guide

--- frontend_crawlers/src/views/Auth/Login.vue
+++ frontend_crawlers/src/views/Auth/Login.vue
@@ -130,7 +130,7 @@ export default {
                 .dispatch("auth/login", this.model)
                 .then(
                     (resolve) => {
-                        _this.$router.push("profile")
+                        _this.$router.push("guide")
                         _this.loading = false
                     },
                     (reject) => {
